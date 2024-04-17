d8ae0b4bb0c25fa192b93c249e3a5974732435d8
kratin aggarwal <kratinaggarwal8750@gmail.com>
2024-04-17 09:47:42 +0530

    Fix import statement in Shop.jsx

--- Frontend/src/Components/Shop.jsx
+++ Frontend/src/Components/Shop.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
-import { CardThree } from "./card";
-
+import { CardThree } from "./Card";
 
 const Shop = () => {
   const [books, setBooks] = useState([]);
@@ -13,19 +12,14 @@ const Shop = () => {
         setBooks(data);
       });
   }, []);
-  console.log(import.meta.env.VITE_API_URL)
+  console.log(import.meta.env.VITE_API_URL);
   return (
     <div className="mt-28 px-4 lg:px-24">
       <h2 className=" text-5xl font-bold text-center">All Books Are Here</h2>
       <div className="grid  grid-cols-1 md:grid-cols-3 my-12 lg:grid-cols-4 sm:grid-cols-2 gap-8">
-        {
-           books.map((book) => (
-            <CardThree book={book} key={book._id} />
-          ))
-
-        }
-      
-
+        {books.map((book) => (
+          <CardThree book={book} key={book._id} />
+        ))}
       </div>
     </div>
   );
